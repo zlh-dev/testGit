f8a7cddb052a679a86c3eea2f4d794fb5dc1728d
zlh-dev <zlh@dev.com>
2024-06-24 16:37:52 +0800

    second commit

--- src/main/java/com/zlh/controller/UserController.java
+++ src/main/java/com/zlh/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.zlh.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @PostMapping("/")
+    public void t1(){
 
+    }
 }
